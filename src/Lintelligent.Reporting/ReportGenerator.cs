4550587072a124103f52ee2889ca83ef5a4012c9
Alexander Markussen <alexander.markussen@live.dk>
2025-12-22 09:33:18 +0100

    Initial commit from Specify template

---
+++ src/Lintelligent.Reporting/ReportGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Lintelligent.AnalyzerEngine;
+using Lintelligent.AnalyzerEngine.Results;
+
+namespace Lintelligent.Reporting;
+
+public class ReportGenerator
+{
+    public string GenerateMarkdown(IEnumerable<DiagnosticResult> results)
+    {
+        // Compose final document using a raw interpolated string and concatenate entries
+        return $"""
+                # Lintelligent Report
+
+                {CollectOutput(results)}
+                """;
+    }
+
+    private static string CollectOutput(IEnumerable<DiagnosticResult> results)
+    {
+        return string.Concat(results.Select(result => $"""
+                                                       - **File:** {result.FilePath}
+                                                         - **Rule:** {result.RuleId}
+                                                         - **Message:** {result.Message}
+                                                         - **Line:** {result.LineNumber}
+
+                                                       """
+        ));
+    }
+}
